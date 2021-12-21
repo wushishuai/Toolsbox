84d28ac498c0ab8907ec8613506fb98505184435
wushishuai <32587860+wushishuai@users.noreply.github.com>
2021-12-21 16:58:43 +0800

    add compass function

--- app/src/main/java/com/example/toolsbox/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/toolsbox/ui/gallery/GalleryFragment.java
@@ -17,15 +17,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.example.toolsbox.CompassActivity;
 import com.example.toolsbox.QrActivity;
 import com.example.toolsbox.R;
 import com.example.toolsbox.databinding.FragmentGalleryBinding;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements View.OnClickListener{
 
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
     private Button QR_generation_button;
+    private Button compass_button;
 //    private FragmentManager manager;
 //    private FragmentTransaction ft;
 
@@ -39,23 +41,26 @@ public class GalleryFragment extends Fragment {
 
 //        final TextView textView = binding.btnTools;
         QR_generation_button = binding.QRButton;
-        QR_generation_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                manager = getFragmentManager();
-//                QRCodeFragment qrCodeFragment = new QRCodeFragment();
-//                ft = manager.beginTransaction();
-//                ft.replace(R.id.nav_gallery,qrCodeFragment);
-//                ft.addToBackStack(null);
-//                ft.commit();
-//                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_nav_qr);
-                Intent intent = new Intent(getActivity(), QrActivity.class);
-                startActivity(intent);
-
-
-
-            }
-        });
+        compass_button = binding.compassButton;
+        QR_generation_button.setOnClickListener(this);
+        compass_button.setOnClickListener(this);
+//        QR_generation_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                manager = getFragmentManager();
+////                QRCodeFragment qrCodeFragment = new QRCodeFragment();
+////                ft = manager.beginTransaction();
+////                ft.replace(R.id.nav_gallery,qrCodeFragment);
+////                ft.addToBackStack(null);
+////                ft.commit();
+////                Navigation.findNavController(view).navigate(R.id.action_nav_gallery_to_nav_qr);
+//                Intent intent = new Intent(getActivity(), QrActivity.class);
+//                startActivity(intent);
+//
+//
+//
+//            }
+//        });
 //        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
@@ -69,5 +74,23 @@ public class GalleryFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.QR_button:
+                Intent intent = new Intent(getActivity(), QrActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.compass_button:
+                Intent intent1 = new Intent(getActivity(), CompassActivity.class);
+                startActivity(intent1);
+                break;
+            default:
+                break;
+
+        }
+
     }
 }
