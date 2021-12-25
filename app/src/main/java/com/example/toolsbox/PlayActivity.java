e66b664492867d3d979c411d7ad47e02942e0a2b
wushishuai <32587860+wushishuai@users.noreply.github.com>
2021-12-25 18:56:00 +0800

    vip parse

---
+++ app/src/main/java/com/example/toolsbox/PlayActivity.java
@@ -0,0 +1,86 @@
+package com.example.toolsbox;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.toolsbox.function.AdFilterTool;
+
+public class PlayActivity extends AppCompatActivity {
+    private String url;
+    private WebView webView_play;
+    private Intent intent;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_play);
+        initWebView();
+    }
+    @Override
+    protected void onResume() {
+        //横屏显示
+        if (getRequestedOrientation()!= ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        super.onResume();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void initWebView(){
+        intent = getIntent();
+        url = intent.getStringExtra("url");
+        webView_play = findViewById(R.id.webView_play);
+        //获取webview的配置
+        WebSettings settings = webView_play.getSettings();
+        //配置支持domstorage
+        settings.setDomStorageEnabled(true);//启用或禁用DOM缓存
+        settings.setAppCacheEnabled(false);//关闭/启用应用缓存
+        settings.setSupportZoom(true);//是否可以缩放，默认true
+        //settings.setBuiltInZoomControls(false);//是否显示缩放按钮，默认false
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowContentAccess(true);
+        settings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        settings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setPluginState(WebSettings.PluginState.ON);
+        settings.setUseWideViewPort(true); // 关键点
+        settings.setDomStorageEnabled(true);
+
+        settings.setAllowFileAccess(true); // 允许访问文件
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        webView_play.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView_play.setWebChromeClient(new WebChromeClient());
+        webView_play.setWebViewClient(new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView webView, String url) {
+                //判断是否是广告相关的资源链接
+                if (!AdFilterTool.isAd(webView.getContext(), url)) {
+                    //这里是不做处理的数据
+                    return super.shouldInterceptRequest(webView, url);
+                } else {
+                    //有广告的请求数据，我们直接返回空数据，注：不能直接返回null
+                    return new WebResourceResponse(null, null, null);
+                }
+            }
+        });
+
+        webView_play.loadUrl(url);
+    }
+}
--- app/src/main/java/com/example/toolsbox/VideoActivity.java
+++ app/src/main/java/com/example/toolsbox/VideoActivity.java
@@ -2,6 +2,7 @@ package com.example.toolsbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,165 +24,59 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class VideoActivity extends AppCompatActivity implements View.OnClickListener {
-    private JzvdStd jzvdStd;
-    private EditText vip_url;
+
     private Button vip_button;
+    private Button vip_button2;
+    private Button vip_button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
-        vip_url = (EditText) findViewById(R.id.vip_text);
         vip_button = (Button) findViewById(R.id.video_button);
+        vip_button2 = (Button) findViewById(R.id.video_button2);
+        vip_button3 = (Button) findViewById(R.id.video_button3);
         vip_button.setOnClickListener(this);
+        vip_button2.setOnClickListener(this);
+        vip_button3.setOnClickListener(this);
 
-        jzvdStd = (JzvdStd) findViewById(R.id.jz_video);
-//        jzvdStd.setUp("https://qq-qy-yk-mg-bl-1905.jjyl12349.com/20210814/SnVWl0JC/index.m3u8"
-//                , "饺子闭眼睛");
-//        jzvdStd.posterImageView.setImage("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
-//        jzvdStd.posterImageView.setImageURI(Uri.parse("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640"));
     }
-    @Override
-    public void onBackPressed() {
-        if (Jzvd.backPress()) {
-            return;
-        }
-        super.onBackPressed();
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Jzvd.releaseAllVideos();
-    }
+//    @Override
+//    public void onBackPressed() {
+//
+//    }
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//    }
 
     @Override
     public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), ViewActivity.class);
+
         switch (view.getId()){
             case R.id.video_button:
-                String url = vip_url.getText().toString();
-                String parse_url = "https://660e.com/?url=https://v.qq.com/x/cover/mzc0020020cyvqh/x0041wg8yjd.html" ;
-//                try {
-//                    Document doc = Jsoup.connect(parse_url).get();
-//                    String txt = doc.text();
-//                    Log.e("-----------",txt);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                Thread parse = new ParseHtml(parse_url);
-//                ParseHtml pase = new ParseHtml(parse_url);
-//                new Thread(pase).start();
-//                String back_txt = pase.getUrl();
-//                Log.e("------",back_txt);
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-//                            Document document = Jsoup.connect(parse_url).get();
-//                            Document document = Jsoup.parse(new URL(parse_url), 100000);
-//
-//                            String txt = document.text();
-                            OkHttpClient client = new OkHttpClient();
-                            Request request = new Request.Builder()
-                                    .url(parse_url)
-                                    .build();
-                            Response response = client.newCall(request).execute();
-                            String txt = response.body().string();
-                            Log.e("----------",txt);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }).start();
-
+                Log.e("---------","hhhhhhhhhhhhh");
+                intent.putExtra("url", "https://v.qq.com/");
+                startActivity(intent);
+                break;
+            case R.id.video_button2:
+                intent.putExtra("url", "https://vip.iqiyi.com/");
+                startActivity(intent);
+                break;
+            case R.id.video_button3:
+                intent.putExtra("url", "https://www.youku.com/");
+                startActivity(intent);
                 break;
             default:
                 break;
 
-//                Log.i("-----------",parse_url);
-//                try {
-//                    ParseHtml.parse();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                jzvdStd.setUp("https://660e.com/?url=https://v.qq.com/x/cover/mzc00200lxzhhqz/d0040q5zhb7.html"
-//                        , "饺子闭眼睛");
+
+
+
+
 
         }
 
     }
 }
-//class ParseHtml implements Runnable{
-//    private String url;
-//    private String txt;
-//    public ParseHtml(String url){
-//        this.url =url;
-//    }
-//    @Override
-//    public void run() {
-//        try {
-//            Log.e("-----------------","11111111111");
-//            Document doc = Jsoup.connect(url).get();
-//            txt = doc.text();
-//            Log.e("---------------","2222222222222222");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        //                try {
-////                    Document doc = Jsoup.connect(parse_url).get();
-////                    String txt = doc.text();
-////                    Log.e("-----------",txt);
-////                } catch (IOException e) {
-////                    e.printStackTrace();
-////                }
-//
-//
-//
-//    }
-//    public String getUrl(){
-//        return txt;
-//    }
-////    public static void parse() throws IOException {
-////        int id = 0;
-////        String u = "https://www.htu.edu.cn/";
-////        String url = "https://www.htu.edu.cn/9547/list.htm";
-////        Document document = Jsoup.parse(new URL(url), 10000);
-////        Elements content = document.getElementsByClass("news_box");
-//////        System.out.println(content);
-////
-////
-////        for (Element href : content) {
-////            String hrefs =  u + href.attr("href");
-//////            System.out.println(hrefs);
-////            Document document1 = Jsoup.parse(new URL(hrefs), 10000);
-////            Elements info_img = document1.getElementsByClass("info_img");
-//////            System.out.println(info_img);
-////            String src = info_img.select("img[src]").attr("src");
-//////            System.out.println(links);
-//////            System.out.println(u + src);
-////            Log.e("---------",src);
-//
-//
-////            URL target = new URL(u + src);
-////            URLConnection urlConnection = target.openConnection();
-////            InputStream inputStream = urlConnection.getInputStream();
-////            id++;
-////            OutputStream outputStream = new FileOutputStream("./Download/" + id + ".jpg");
-////            int temp = 0;
-////            while((temp = inputStream.read()) != -1) {
-////                outputStream.write(temp);
-////            }
-////            System.out.println(id + ".jpg 下载完毕");
-////            outputStream.close();
-////            inputStream.close();
-////        }
-////
-////
-////
-////    }
-//
-////    @Override
-////    public void run() {
-////
-////    }
-//}
-//
